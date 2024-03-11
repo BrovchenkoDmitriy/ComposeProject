4e2529c4ac3552bde37d06a6382250a904ba36f3
BrovchenkoDmitriy <99586855+BrovchenkoDmitriy@users.noreply.github.com>
2024-03-11 17:27:17 +0900

    work with clickListeners - create VkNewsMainScreenNew.kt with and work with content of VkNewsCard. Work with clickListeners of VkNewsCard elements.

---
+++ app/src/main/java/com/example/composeproject/domain/FeedPost.kt
@@ -0,0 +1,18 @@
+package com.example.composeproject.domain
+
+import com.example.composeproject.R
+
+data class FeedPost(
+    val communityName: String = "уволено",
+    val publicationDate: String = "14:20",
+    val avatarResId: Int = R.drawable.ic_launcher_foreground,
+    val contentText: String = "Просто обычный текст, не несущий смысловой нагрузки и предназначенный для теста.",
+    val contentImageResId: Int = R.drawable.ic_launcher_background,
+    val statistics: List<StatisticItem> = listOf(
+        StatisticItem(type = StatisticType.VIEWS, count = 206),
+        StatisticItem(type = StatisticType.SHARES, 96),
+        StatisticItem(type = StatisticType.COMMENTS, 57),
+        StatisticItem(type = StatisticType.LIKES, 13),
+    )
+
+)
